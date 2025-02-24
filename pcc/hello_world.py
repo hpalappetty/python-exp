7dac1b70ef21ff375754f98aa8efb830ef331c00
hpalappetty <hpalappe@gmail.com>
2025-02-24 12:05:27 +0000

    more examples

--- pcc/hello_world.py
+++ pcc/hello_world.py
@@ -2,3 +2,7 @@ print("Hello Python World")
 
 message = "Hello Python World"
 print(message)
+
+message = "This is Hari"
+print(message)
+ 
---
+++ pcc/variables_sdt.py
@@ -0,0 +1,16 @@
+name = "ada lovelace"
+print(name.title())
+
+print(name.upper())
+print(name.lower())
+
+# variables in string
+
+first_name = "ada"
+last_name = "lovelace"
+
+full_name = f"{first_name} {last_name}"
+print(full_name.title())
+
+full_name = f"Hello, {first_name} {last_name}!"
+print(full_name.title())
